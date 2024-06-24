0d641df8257620989f685725dfe6e3e7ec02babd
Andrei Maroiu <achimaroiu@yahoo.com>
2024-06-24 15:13:29 +0300

    added solution to exercises

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -4,11 +4,8 @@
 //
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
-
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = [5; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -16,4 +13,9 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed")
     }
+
+    for i in a {
+        print!("{} ", i);
+    }
+        
 }
